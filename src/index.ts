b44ea939a8999ff976eec11d6c4b3594021d0163
Dominique Rochefort <drochefort@tribalnova.com>
2018-07-31 16:52:12 -0400

    setup mocha-headless chrome

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { Options, Run, runner } from 'mocha-headless-chrome';
+
+const options: Options = {
+    file: 'test/runner.html', // test page path
+    reporter: 'text', // mocha reporter name
+    width: 800, // viewport width
+    height: 600, // viewport height
+    timeout: 120000, // timeout in ms
+    args: ['no-sandbox'] // chrome arguments
+};
+
+process.on('unhandledRejection', (reason, p) => {
+    console.log('Unhandled Rejection at:', p, 'reason:', reason);
+    // application specific logging, throwing an error, or other logic here
+});
+
+(async () => {
+    const result: Run = await runner(options);
+    const json: string = JSON.stringify(result);
+    console.log(json);
+})();
