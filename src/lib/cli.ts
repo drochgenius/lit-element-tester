f16e6194e9abe83d04a30024f3c9c8cf8eedf095
Dominique Rochefort <drochefort@tribalnova.com>
2018-08-01 17:02:14 -0400

    fine tune coverage and options

--- src/lib/cli.ts
+++ src/lib/cli.ts
@@ -1,19 +1,44 @@
 #!/usr/bin/env node
+import { Options } from 'mocha-headless-chrome';
 import * as program from 'commander';
-import { run } from './index';
+import { instrument, run } from './index';
 import { serve } from './server';
 
 program
     .version('0.0.1')
-    .option('-d, --development', 'launch a real browser for development')
+    .usage('<files...> [options]')
+    .option('-f, --file [file]', 'html runner file path')
+    .option('-d, --development', 'launch a real browser for test development')
+    .option('-H --height [px]', 'browser window height in pixels', parseInt, 800)
+    .option('-W, --width [px]', 'browser window width in pixels', parseInt, 600)
+    .option('-T, --timeout [ms]', 'test timeout in milliseconds', parseInt, 120000)
+    .option('-R, --reporter [name]', 'mocha reporter', 'text')
     .parse(process.argv);
 
 const args: string[] = program.args;
 
 console.log('args', args);
+console.log('file', program.file);
 
-if (program.development) {
-    serve(args[0]);
-} else {
-    run(args[0]);
-}
+(async (): Promise<void> => {
+    const options: Options = {
+        file: program.file || 'test/runner.html', // test page path
+        reporter: program.reporter || 'text', // mocha reporter name
+        width: program.width || 800, // viewport width
+        height: program.height || 600, // viewport height
+        timeout: program.timeout || 120000, // timeout in ms
+        args: ['no-sandbox'] // chrome arguments
+    };
+
+    // Generate instrumented files for coverage
+    if (args) {
+        await instrument(args);
+    }
+
+    // Run the tests
+    if (program.development) {
+        serve(program.file);
+    } else {
+        await run(options);
+    }
+})();
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,27 +1,40 @@
 import { Options, Run, runner } from 'mocha-headless-chrome';
+import * as istanbul from 'istanbul';
+import { readFileSync, writeFileSync } from 'fs';
 // @ts-ignore
 import { remap, writeReport } from 'remap-istanbul';
-import * as istanbul from 'istanbul';
+
 process.on('unhandledRejection', (reason, p) => {
     console.log('Unhandled Rejection at:', p, 'reason:', reason);
     // application specific logging, throwing an error, or other logic here
 });
 
-// const collector = new istanbul.Collector();
+const instrumenter: istanbul.Instrumenter = new istanbul.Instrumenter();
 const reporter: istanbul.Reporter = new istanbul.Reporter();
 
-export async function run(file: string = 'test/runner.html'): Promise<void> {
-    const options: Options = {
-        file, // test page path
-        reporter: 'text', // mocha reporter name
-        width: 800, // viewport width
-        height: 600, // viewport height
-        timeout: 120000, // timeout in ms
-        args: ['no-sandbox'] // chrome arguments
-    };
+/**
+ * Instrument Javascript files for code coverage reporting
+ *
+ * @param files : a list of javascript files to instrument with Istanbul
+ */
+export async function instrument(files: string[] = []) {
+    for (const sourceFile of files) {
+        if (!sourceFile.includes('$')) {
+            const instrumentedFile = sourceFile.replace(/\.js$/, '.$.js');
+            const code: string = readFileSync(sourceFile, 'utf8');
+            const instrumented: string = instrumenter.instrumentSync(code, sourceFile);
+            writeFileSync(instrumentedFile, instrumented, 'utf8');
+        }
+    }
+}
 
+function exclude(file: string): boolean {
+    return file.includes('node_modules') || file.includes('bootstrap');
+}
+
+export async function run(options: Options): Promise<void> {
     const { coverage }: Run = await runner(options);
-    const remappedCoverage: any = remap(coverage);
+    const remappedCoverage: any = remap(coverage, { exclude });
 
     reporter.add('text');
     reporter.write(remappedCoverage, true, function() {
