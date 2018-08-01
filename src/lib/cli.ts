f889b18457e6bf510aaef443772725dbafe4ffdd
Dominique Rochefort <drochefort@tribalnova.com>
2018-08-01 17:16:20 -0400

    add example to cli help page

--- src/lib/cli.ts
+++ src/lib/cli.ts
@@ -6,6 +6,7 @@ import { serve } from './server';
 
 program
     .version('0.0.1')
+    .description('Run unit tests for custom elements in Google Chrome')
     .usage('<files...> [options]')
     .option('-f, --file [file]', 'html runner file path')
     .option('-d, --development', 'launch a real browser for test development')
@@ -13,6 +14,15 @@ program
     .option('-W, --width [px]', 'browser window width in pixels', parseInt, 600)
     .option('-T, --timeout [ms]', 'test timeout in milliseconds', parseInt, 120000)
     .option('-R, --reporter [name]', 'mocha reporter', 'text')
+    .on('--help', function() {
+        console.log(`
+  Examples:
+    
+  (1) Run the test and collect coverage for all javascript files in dist/ folder.
+
+    $ custom-element-tester dist/*.js -f test/index.html
+        `)
+    })
     .parse(process.argv);
 
 const args: string[] = program.args;
